29c09bc5cd90b05df7f2e2ca1eeeb00fc66b7f48
NsikanPatrick <nsikanpatrick69@gmail.com>
2024-06-24 14:08:48 +0200

    Improved the chat.py file to save responses to the json knowledge base

--- chat.py
+++ chat.py
@@ -1,5 +1,56 @@
-import torch
-import nltk
+import json
+from difflib import get_close_matches
 
-# nltk.download('punkt')
-# print(torch.__version__)
+
+# Load the knowledge abse from a json file
+def load_knowledge_base(file_path: str) -> dict:
+    with open(file_path, 'r') as file:
+        data: dict = json.load(file)
+
+    return data
+
+# Save the updated knowledge base file
+def save_knowledge_base(file_path: str, data:dict):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=2)
+
+def find_best_match(user_question: str, questions:list[str]) -> str | None:
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+    return matches[0] if matches else None
+
+def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
+    for q in knowledge_base["questions"]:
+        if q["question"] == question:
+            return q["answer"]
+        # return None
+    
+def chat_bot():
+    knowledge_base: dict = load_knowledge_base('knowledge_base.json')
+
+    while True:
+        user_input: str = input('You: ')
+
+        if user_input.lower() == 'quit':
+            break
+
+        best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+
+        if best_match:
+            answer: str = get_answer_for_question(best_match, knowledge_base)
+
+            print(f'Bot: {answer}')
+
+        else:
+            print(f'Bot: I don\'t know the answer, can you teach me?')
+
+            new_answer: str = input('Type the answer or "Skip" to continue: ')
+
+            if new_answer.lower() != 'skip':
+                knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
+                save_knowledge_base("knowledge_base.json", knowledge_base)
+                print(f'Bot: Thank you, I learnt a new response')
+
+
+
+if __name__ == '__main__':
+    chat_bot()
--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 
 stemmer = PorterStemmer()
 
@@ -12,15 +13,37 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    """
+    sentence = ["Hello", "How", "Are", "You"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag = [0, 1, 0, 1, 0, 0, 0]
+    """
+    # What happened is that we pick each word from sentence array and check it in the
+    # words array, we place 1 in the bag array at the same position were it is in the 
+    # words array
 
-q = "How long does shipping take?"
-print(q)
-q = tokenize(q)
-print(q)
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
 
-ans = ["Shipping", "takes", "14", "days"]
-stemmed_words = [stem(a) for a in ans]
-print(stemmed_words)
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, word in enumerate(all_words):
+        if word in tokenized_sentence:
+            bag[idx] = 1.0
+            
+    return bag
+    
+sentence = ["Hello", "How", "Are", "You"]
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bag = bag_of_words(sentence, words)
+
+# print(bag)
+
+# q = "How long does shipping take?"
+# print(q)
+# q = tokenize(q)
+# print(q)
+
+# ans = ["Shipping", "takes", "14", "days"]
+# stemmed_words = [stem(a) for a in ans]
+# print(stemmed_words)
 
 
